fd3534e5318dbed8b5dd5c779698be9ca6beb80c
弄潮者 <497128887@qq.com>
2015-10-26 19:39:23 +0800

    APP基本功能均已完成
    APP基本功能均已完成

---
+++ OhOne_已加分享接口:缓存:登陆页面/OhOne/Main/Base/BaseViewController.h
@@ -0,0 +1,23 @@
+//
+//  BaseViewController.h
+//  OhOne
+//
+//  Created by hyrMac on 15/8/22.
+//  Copyright (c) 2015年 hyrMac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "ShareView.h"
+
+@interface BaseViewController : UIViewController
+
+
+@property (nonatomic, strong) UIButton *rightButton;
+
+@property (nonatomic, strong) UIControl *coverView;
+@property (nonatomic, strong) ShareView *shareView;
+
+
+- (void)coverTapAction:(UIControl *)control;
+
+@end
